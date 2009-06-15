5e12455a4ca57b613f70f23f1828558bb9347ea6
David Balatero <dbalatero@evri.com>
2009-06-15 13:08:32 -0700

    Fixed specs for https.

--- spec/evri/rpx/session_spec.rb
+++ spec/evri/rpx/session_spec.rb
@@ -18,7 +18,7 @@ describe Evri::RPX::Session do
 
     it "should raise an APICallError if RPX returns an error message" do
       FakeWeb.register_uri(:get,
-                           'http://rpxnow.com:443/api/v2/auth_info',
+                           'https://rpxnow.com:443/api/v2/auth_info',
                            :file => fixture_path('session/normal_error.json'),
                            :status => ['400', 'Bad Request'])
       lambda {
@@ -28,7 +28,7 @@ describe Evri::RPX::Session do
 
     it "should raise ServiceUnavailableError if the service is not available" do
       FakeWeb.register_uri(:get,
-                           'http://rpxnow.com:443/api/v2/auth_info',
+                           'https://rpxnow.com:443/api/v2/auth_info',
                            :file => fixture_path('session/service_down_error.json'),
                            :status => ['404', 'Not Found'])
       lambda {
@@ -38,7 +38,7 @@ describe Evri::RPX::Session do
 
     it "should return a User object for a mapping" do
       FakeWeb.register_uri(:get,
-                           'http://rpxnow.com:443/api/v2/auth_info',
+                           'https://rpxnow.com:443/api/v2/auth_info',
                            :file => fixture_path('user/dbalatero_gmail.json'))
 
       result = @session.auth_info('mytoken')
@@ -49,20 +49,20 @@ describe Evri::RPX::Session do
   describe "map" do
     before(:each) do
       FakeWeb.register_uri(:get,
-                           'http://rpxnow.com:443/api/v2/map',
+                           'https://rpxnow.com:443/api/v2/map',
                            :file => fixture_path('session/map.json'))
     end
 
     it "should take in a User object as the second parameter" do
       user = mock('user')
-      user.should_receive(:identifier).and_return('http://www.facebook.com/dbalatero')
+      user.should_receive(:identifier).and_return('https://www.facebook.com/dbalatero')
 
       result = @session.map(user, 50)
       result.should be_true
     end
 
     it "should take in a identifier string as the second parameter" do
-      result = @session.map('http://www.facebook.com/dbalatero', 50)
+      result = @session.map('https://www.facebook.com/dbalatero', 50)
       result.should be_true
     end
   end
@@ -70,7 +70,7 @@ describe Evri::RPX::Session do
   describe "mappings" do
     it "should return a Mappings object" do
       FakeWeb.register_uri(:get,
-                           'http://rpxnow.com:443/api/v2/mappings',
+                           'https://rpxnow.com:443/api/v2/mappings',
                            :file => fixture_path('mappings/identifiers.json'))
 
       result = @session.mappings('dbalatero')
@@ -80,7 +80,7 @@ describe Evri::RPX::Session do
 
     it "should take a User object in" do
       FakeWeb.register_uri(:get,
-                           'http://rpxnow.com:443/api/v2/mappings',
+                           'https://rpxnow.com:443/api/v2/mappings',
                            :file => fixture_path('mappings/identifiers.json'))
       user = mock('user')
       user.should_receive(:primary_key).and_return('dbalatero')
@@ -94,18 +94,18 @@ describe Evri::RPX::Session do
   describe "unmap" do
     before(:each) do
       FakeWeb.register_uri(:get,
-                           'http://rpxnow.com:443/api/v2/unmap',
+                           'https://rpxnow.com:443/api/v2/unmap',
                            :file => fixture_path('session/unmap.json'))
     end
 
     it "should take a string as the identifier" do
-      result = @session.unmap('http://www.facebook.com/dbalatero', 50)
+      result = @session.unmap('https://www.facebook.com/dbalatero', 50)
       result.should be_true
     end
 
     it "should take a User as the identifier" do
       user = mock('user')
-      user.should_receive(:identifier).and_return('http://www.facebook.com/dbalatero')
+      user.should_receive(:identifier).and_return('https://www.facebook.com/dbalatero')
 
       result = @session.unmap(user, 50)
       result.should be_true
